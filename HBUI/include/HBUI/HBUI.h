778e356a008ed4db175e25e185b82aebe63006e1
Kasper de Bruin <bruinkasper@gmail.com>
2024-02-06 22:22:59 +0100

    Add HBUI

---
+++ HBUI/include/HBUI/HBUI.h
@@ -0,0 +1,14 @@
+//
+// Created by Kasper de Bruin on 06/02/2024.
+//
+
+#ifndef HUMMINGBIRD_HBUI_H
+#define HUMMINGBIRD_HBUI_H
+
+#include <imgui.h>
+
+namespace HBUI {
+  class HBUI {
+  };
+}// namespace HBUI
+#endif//HUMMINGBIRD_HBUI_H
